f1357f49a7eddc07d8972a837fd709fd04360452
udaypydi <udaypydi333@gmail.com>
2020-02-11 02:25:00 +0530

    feature(introduction): introduction added

---
+++ src/components/Introduction/Introduction.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Introduction.scss";
+
+const Introduction = (props) => {
+    return (
+        <div className="Introduction">
+            <div className="Introduction__text">
+                <h1 className="Introduction__header">Hi,</h1>
+                <h2 className="Introduction__name">This is Uday Bhaskhar</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Introduction from './Introduction/Introduction.component';
+
+const Components = {
+    Introduction,
+};
+
+export default Components;
