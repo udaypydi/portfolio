dc0180a8d830037780581b776a4c117ca86eac92
uday <uday@udays-MacBook-Pro.local>
2021-02-16 22:51:43 +0530

    fix(socialprofiles): add resume icon

--- src/components/SocialProfiles/SocialProfiles.component.js
+++ src/components/SocialProfiles/SocialProfiles.component.js
@@ -9,8 +9,9 @@ export default function SocialProfiles() {
                 SocialProfilesData.map(profile => (
                     <img 
                         onClick={() => window.open(profile.link, '_blank')}
-                        src={require(`../../assets/icons/${profile.icon}`)} 
+                        src={profile?.remoteUrl ? profile.icon : require(`../../assets/icons/${profile.icon}`)} 
                         className="SocialProfiles--Image" 
+                        style={profile?.remoteUrl ? { height: 50, marginLeft: '-10px' } : {}}
                     />
                 ))
             }
--- src/components/SocialProfiles/SocialProfiles.constant.js
+++ src/components/SocialProfiles/SocialProfiles.constant.js
@@ -1,26 +1,33 @@
 export const SocialProfiles = [
-    {
-      "id": 1,
-      "name": "Github",
-      "link": "https://github.com/udaypydi",
-      "icon": "github.svg"
-    },
-    {
-      "id": 2,
-      "name": "Linkedin",
-      "link": "https://www.linkedin.com/in/udaypydi/",
-      "icon": "linkedin.svg"
-    },
-    {
-      "id": 0,
-      "name": "Medium",
-      "link": "https://medium.com/@udaybhaskhar",
-      "icon": "medium.svg"
-    },
-    {
-      "id": 3,
-      "name": "Twitter",
-      "link": "https://twitter.com/uday_bhaskhar",
-      "icon": "twitter.svg"
-    }
-  ]
+  {
+    "id": 1,
+    "name": "Github",
+    "link": "https://github.com/udaypydi",
+    "icon": "github.svg"
+  },
+  {
+    "id": 2,
+    "name": "Linkedin",
+    "link": "https://www.linkedin.com/in/udaypydi/",
+    "icon": "linkedin.svg"
+  },
+  {
+    "id": 0,
+    "name": "Medium",
+    "link": "https://medium.com/@udaybhaskhar",
+    "icon": "medium.svg"
+  },
+  {
+    "id": 3,
+    "name": "Twitter",
+    "link": "https://twitter.com/uday_bhaskhar",
+    "icon": "twitter.svg"
+  },
+  {
+    "id": 4,
+    "name": "Resume",
+    "link": "https://res.cloudinary.com/dgvup74b7/image/upload/v1613494770/Uday_Resume_d81pvn.pdf",
+    "icon": "https://res.cloudinary.com/dgvup74b7/image/upload/v1613494610/icons8-pdf-96_tdb3vj.png",
+    "remoteUrl": true
+  }
+]
